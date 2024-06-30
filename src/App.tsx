5ba755fe35a6690622e7731a1e94d9446c92ee50
luiscastro8 <ochoad64@gmail.com>
2024-06-30 12:19:23 -0500

    started connecting to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import {Button, TextField} from "@mui/material";
+import {useState} from 'react'
+import {Button, CircularProgress, TextField} from "@mui/material";
+
+const BACKEND_URL = ""
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [address, setAddress] = useState("")
 
-  return (
-    <>
-        <TextField label="IP Address"/>
-        <Button variant="contained">Submit</Button>
-    </>
-  )
+    const getServerInfo = async () => {
+        setLoading(true)
+        try {
+            await fetch(BACKEND_URL)
+        } catch (e) {
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <>
+            <TextField label="IP Address" value={address} onChange={(event) => {
+                setAddress(event.target.value)
+            }}/>
+            <Button variant="contained" onClick={getServerInfo}>Submit</Button>
+            {loading && <CircularProgress/>}
+        </>
+    )
 }
 
 export default App
