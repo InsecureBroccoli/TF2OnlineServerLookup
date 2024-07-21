c84fcb03de63eab460e731302d79b390a4fb1ce8
InsecureBroccoli <ochoad64+broccoli@gmail.com>
2024-07-21 11:24:37 -0500

    add ip input validation to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,14 @@ function App() {
     const [loading, setLoading] = useState(false)
     const [address, setAddress] = useState("")
     const [serverData, setServerData] = useState(null)
+    const [addressValidationError, setAddressValidationError] = useState(false)
 
     const getServerInfo = async () => {
+        if (!validateAddress(address)) {
+            setAddressValidationError(true)
+            return
+        }
+
         setLoading(true)
         fetch(`${BACKEND_URL}/api/v1/getserverinfo?address=${address}`)
             .then((response) => response.json())
@@ -17,10 +23,21 @@ function App() {
             .finally(() => setLoading(false))
     }
 
+    const validateAddress = (address: string): boolean => {
+        const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(:([0-9]{1,5}))?$/;
+        const domainRegex = /^(?=.{1,253}$)(?!-)([A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
+
+        return ipv4Regex.test(address) || domainRegex.test(address);
+    }
+
     return (
         <>
-            <TextField label="IP Address" value={address} onChange={(event) => {
+            <TextField error={addressValidationError}
+                       helperText={addressValidationError && "Invalid IP Address"}
+                       label="IP Address" value={address}
+                       onChange={(event) => {
                 setAddress(event.target.value)
+                setAddressValidationError(false)
             }}/>
             <Button variant="contained" onClick={getServerInfo}>Submit</Button>
             {loading && <CircularProgress/>}
